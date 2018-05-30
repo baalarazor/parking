a778db44a07ca8f7b75e88d1ecf1475d5230bfb5
Alejandro Verbo <alejandro.verbo@atos.net>
2018-05-30 11:32:19 +0200

    Model classes and repositories

---
+++ src/main/java/com/example/parking/model/Parking.java
@@ -0,0 +1,66 @@
+package com.example.parking.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import io.swagger.annotations.ApiModel;
+
+@Entity
+@ApiModel(description="All details about the parking. ")
+public class Parking {
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private Integer size;
+	
+	@OneToMany(mappedBy = "parking", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private Set<ParkingBay> bays = new HashSet<ParkingBay>();
+	
+	public Parking() {
+		super();
+	}
+
+	public Parking(Integer size) {
+		super();
+		this.size = size;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getSize() {
+		return size;
+	}
+
+	public void setSize(Integer size) {
+		this.size = size;
+	}
+
+	public void addBay(ParkingBay bay) {
+		this.getBays().add(bay);
+	}
+
+	public Set<ParkingBay> getBays() {
+		return bays;
+	}
+
+	public void setBays(Set<ParkingBay> bays) {
+		this.bays = bays;
+	}
+	
+	
+		
+}
---
+++ src/main/java/com/example/parking/model/ParkingBayRepository.java
@@ -0,0 +1,13 @@
+package com.example.parking.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParkingBayRepository extends JpaRepository<ParkingBay, Long>{
+	
+	int countByParkingIdAndPedestrianExitFalseAndParkedCarNull(Long id);
+	ParkingBay findByParkingIdAndIndex(Long id, Integer index);
+	
+
+}

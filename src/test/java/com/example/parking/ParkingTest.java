32aae730847fe525a4fc460ef35dca275e2a7af3
Alejandro Verbo <alejandro.verbo@atos.net>
2018-06-01 11:06:14 +0200

    Final tests

--- src/test/java/com/example/parking/ParkingTest.java
+++ src/test/java/com/example/parking/ParkingTest.java
@@ -21,7 +21,7 @@ import com.example.parking.model.ParkingRepository;
 public class ParkingTest {
     private static final int FIRSTUPEDESTRIANUEXITUINDEX = 8;
 
-    private Long idParking;
+    private Parking parking;
     
     @Autowired
     private ParkingService parkingService;
@@ -31,106 +31,105 @@ public class ParkingTest {
 
     @Before
     public void setUp() {
-        Parking parking = new ParkingBuilder().withSquareSize(5).withPedestrianExit(FIRSTUPEDESTRIANUEXITUINDEX).withPedestrianExit(12).withDisabledBay(5)
+        parking = new ParkingBuilder().withSquareSize(5).withPedestrianExit(FIRSTUPEDESTRIANUEXITUINDEX).withPedestrianExit(12).withDisabledBay(5)
             .withDisabledBay(10).build();
-        idParking = parking.getId();
         parkingRepository.save(parking);
     }
 
     @Test
     public void testGetAvailableBays() {
-        assertEquals(23, parkingService.getAvailableBays(idParking));
+        assertEquals(23, parkingService.getAvailableBays(parking));
     }
 
     @Test
     public void testParkCarVehiculeTypeC() {
-        assertEquals(Integer.valueOf(7), parkingService.parkCar(idParking, 'C'));
-        assertEquals(22, parkingService.getAvailableBays(idParking));
+        assertEquals(Integer.valueOf(7), parkingService.parkCar(parking, 'C'));
+        assertEquals(22, parkingService.getAvailableBays(parking));
     }
 
     @Test
     public void testParkCarVehiculeTypeM() {
-        assertEquals(Integer.valueOf(7), parkingService.parkCar(idParking, 'M'));
-        assertEquals(22, parkingService.getAvailableBays(idParking));
+        assertEquals(Integer.valueOf(7), parkingService.parkCar(parking, 'M'));
+        assertEquals(22, parkingService.getAvailableBays(parking));
     }
 
     @Test
     public void testParkCarTwoVehicules() {
-    	assertEquals(Integer.valueOf(7), parkingService.parkCar(idParking, 'C'));
-        assertEquals(22, parkingService.getAvailableBays(idParking));
+    	assertEquals(Integer.valueOf(7), parkingService.parkCar(parking, 'C'));
+        assertEquals(22, parkingService.getAvailableBays(parking));
 
-        assertEquals(Integer.valueOf(9), parkingService.parkCar(idParking, 'M'));
-        assertEquals(21, parkingService.getAvailableBays(idParking));
+        assertEquals(Integer.valueOf(9), parkingService.parkCar(parking, 'M'));
+        assertEquals(21, parkingService.getAvailableBays(parking));
     }
 
     @Test
     public void testParkCarDisabled() {
-        assertEquals(Integer.valueOf(10), parkingService.parkCar(idParking, 'D'));
-        assertEquals(22, parkingService.getAvailableBays(idParking));
+        assertEquals(Integer.valueOf(10), parkingService.parkCar(parking, 'D'));
+        assertEquals(22, parkingService.getAvailableBays(parking));
 
-        assertEquals(Integer.valueOf(5), parkingService.parkCar(idParking, 'D'));
-        assertEquals(21, parkingService.getAvailableBays(idParking));
+        assertEquals(Integer.valueOf(5), parkingService.parkCar(parking, 'D'));
+        assertEquals(21, parkingService.getAvailableBays(parking));
 
-        assertEquals(Integer.valueOf(-1), parkingService.parkCar(idParking, 'D'));
-        assertEquals(21, parkingService.getAvailableBays(idParking));
+        assertEquals(Integer.valueOf(-1), parkingService.parkCar(parking, 'D'));
+        assertEquals(21, parkingService.getAvailableBays(parking));
     }
 
     @Test
     public void testUnparkCar() {
-        final int firstCarBayIndex = parkingService.parkCar(idParking, 'C');
-        assertTrue(parkingService.unparkCar(idParking, firstCarBayIndex));
-        assertEquals(23, parkingService.getAvailableBays(idParking));
-        assertFalse(parkingService.unparkCar(idParking, firstCarBayIndex));
+        final int firstCarBayIndex = parkingService.parkCar(parking, 'C');
+        assertTrue(parkingService.unparkCar(parking, firstCarBayIndex));
+        assertEquals(23, parkingService.getAvailableBays(parking));
+        assertFalse(parkingService.unparkCar(parking, firstCarBayIndex));
 
-        final int secondCarBayIndex = parkingService.parkCar(idParking, 'D');
-        assertTrue(parkingService.unparkCar(idParking, secondCarBayIndex));
-        assertEquals(23, parkingService.getAvailableBays(idParking));
-        assertFalse(parkingService.unparkCar(idParking, secondCarBayIndex));
+        final int secondCarBayIndex = parkingService.parkCar(parking, 'D');
+        assertTrue(parkingService.unparkCar(parking, secondCarBayIndex));
+        assertEquals(23, parkingService.getAvailableBays(parking));
+        assertFalse(parkingService.unparkCar(parking, secondCarBayIndex));
 
-        assertFalse(parkingService.unparkCar(idParking, FIRSTUPEDESTRIANUEXITUINDEX));
+        assertFalse(parkingService.unparkCar(parking, FIRSTUPEDESTRIANUEXITUINDEX));
     }
 
     @Test
     public void testToString() {
-        assertEquals("UUUUU\nU=UU@\n@U=UU\nUUUUU\nUUUUU", parkingService.printParking(idParking));
+        assertEquals("UUUUU\nU=UU@\n@U=UU\nUUUUU\nUUUUU", parkingService.printParking(parking));
     }
 
     @Test
     public void testCompleteSolution() {
-        assertEquals(Integer.valueOf(7), parkingService.parkCar(idParking, 'C'));
-        assertEquals("UUUUU\nU=CU@\n@U=UU\nUUUUU\nUUUUU", parkingService.printParking(idParking));
-        assertEquals(22, parkingService.getAvailableBays(idParking));
+        assertEquals(Integer.valueOf(7), parkingService.parkCar(parking, 'C'));
+        assertEquals("UUUUU\nU=CU@\n@U=UU\nUUUUU\nUUUUU", parkingService.printParking(parking));
+        assertEquals(22, parkingService.getAvailableBays(parking));
 
-        assertEquals(Integer.valueOf(9), parkingService.parkCar(idParking, 'C'));
-        assertEquals("UUUUU\nC=CU@\n@U=UU\nUUUUU\nUUUUU", parkingService.printParking(idParking));
-        assertEquals(21, parkingService.getAvailableBays(idParking));
+        assertEquals(Integer.valueOf(9), parkingService.parkCar(parking, 'C'));
+        assertEquals("UUUUU\nC=CU@\n@U=UU\nUUUUU\nUUUUU", parkingService.printParking(parking));
+        assertEquals(21, parkingService.getAvailableBays(parking));
 
-        assertEquals(Integer.valueOf(11), parkingService.parkCar(idParking, 'M'));
-        assertEquals("UUUUU\nC=CU@\n@M=UU\nUUUUU\nUUUUU", parkingService.printParking(idParking));
-        assertEquals(20, parkingService.getAvailableBays(idParking));
+        assertEquals(Integer.valueOf(11), parkingService.parkCar(parking, 'M'));
+        assertEquals("UUUUU\nC=CU@\n@M=UU\nUUUUU\nUUUUU", parkingService.printParking(parking));
+        assertEquals(20, parkingService.getAvailableBays(parking));
 
-        assertEquals(Integer.valueOf(13), parkingService.parkCar(idParking, 'M'));
-        assertEquals("UUUUU\nC=CU@\n@M=MU\nUUUUU\nUUUUU", parkingService.printParking(idParking));
-        assertEquals(19, parkingService.getAvailableBays(idParking));
+        assertEquals(Integer.valueOf(13), parkingService.parkCar(parking, 'M'));
+        assertEquals("UUUUU\nC=CU@\n@M=MU\nUUUUU\nUUUUU", parkingService.printParking(parking));
+        assertEquals(19, parkingService.getAvailableBays(parking));
 
-        assertEquals(Integer.valueOf(10), parkingService.parkCar(idParking, 'D'));
-        assertEquals("UUUUU\nC=CU@\nDM=MU\nUUUUU\nUUUUU", parkingService.printParking(idParking));
-        assertEquals(18, parkingService.getAvailableBays(idParking));
+        assertEquals(Integer.valueOf(10), parkingService.parkCar(parking, 'D'));
+        assertEquals("UUUUU\nC=CU@\nDM=MU\nUUUUU\nUUUUU", parkingService.printParking(parking));
+        assertEquals(18, parkingService.getAvailableBays(parking));
 
-        assertEquals(Integer.valueOf(5), parkingService.parkCar(idParking, 'D'));
-        assertEquals("UUUUU\nC=CUD\nDM=MU\nUUUUU\nUUUUU", parkingService.printParking(idParking));
-        assertEquals(17, parkingService.getAvailableBays(idParking));
+        assertEquals(Integer.valueOf(5), parkingService.parkCar(parking, 'D'));
+        assertEquals("UUUUU\nC=CUD\nDM=MU\nUUUUU\nUUUUU", parkingService.printParking(parking));
+        assertEquals(17, parkingService.getAvailableBays(parking));
 
-        assertEquals(Integer.valueOf(-1), parkingService.parkCar(idParking, 'D'));
-        assertEquals("UUUUU\nC=CUD\nDM=MU\nUUUUU\nUUUUU", parkingService.printParking(idParking));
-        assertEquals(17, parkingService.getAvailableBays(idParking));
+        assertEquals(Integer.valueOf(-1), parkingService.parkCar(parking, 'D'));
+        assertEquals("UUUUU\nC=CUD\nDM=MU\nUUUUU\nUUUUU", parkingService.printParking(parking));
+        assertEquals(17, parkingService.getAvailableBays(parking));
 
-        assertFalse(parkingService.unparkCar(idParking, 3));
-        assertEquals("UUUUU\nC=CUD\nDM=MU\nUUUUU\nUUUUU", parkingService.printParking(idParking));
-        assertEquals(17, parkingService.getAvailableBays(idParking));
+        assertFalse(parkingService.unparkCar(parking, 3));
+        assertEquals("UUUUU\nC=CUD\nDM=MU\nUUUUU\nUUUUU", parkingService.printParking(parking));
+        assertEquals(17, parkingService.getAvailableBays(parking));
 
-        assertTrue(parkingService.unparkCar(idParking, 13));
-        assertEquals("UUUUU\nC=CUD\nDM=UU\nUUUUU\nUUUUU", parkingService.printParking(idParking));
-        assertEquals(18, parkingService.getAvailableBays(idParking));
+        assertTrue(parkingService.unparkCar(parking, 13));
+        assertEquals("UUUUU\nC=CUD\nDM=UU\nUUUUU\nUUUUU", parkingService.printParking(parking));
+        assertEquals(18, parkingService.getAvailableBays(parking));
     }
 }
